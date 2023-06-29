8fbec861cacd4340470efb777c06b73ca3eafd1d
Alexander Fadeev <fadeevab.com@gmail.com>
2023-06-29 14:32:52 +0300

    Move more methods inside of the theme

--- src/theme.rs
+++ src/theme.rs
@@ -46,41 +46,41 @@ impl<R> From<&State<R>> for ThemeState {
     }
 }
 
-fn state_color(state: &ThemeState) -> Style {
-    match state {
-        ThemeState::Active => Style::new().cyan(),
-        ThemeState::Cancel => Style::new().red(),
-        ThemeState::Submit => Style::new().bright().black(),
-        ThemeState::Error(_) => Style::new().yellow(),
-    }
-}
-
-fn state_symbol(state: &ThemeState) -> String {
-    let color = state_color(state);
-    let green = Style::new().green();
-
-    match state {
-        ThemeState::Active => color.apply_to(S_STEP_ACTIVE),
-        ThemeState::Cancel => color.apply_to(S_STEP_CANCEL),
-        ThemeState::Submit => green.apply_to(S_STEP_SUBMIT),
-        ThemeState::Error(_) => color.apply_to(S_STEP_ERROR),
-    }
-    .to_string()
-}
-
-fn cursor_parts_with_style(cursor: &StringCursor, new_style: &Style) -> String {
-    let (left, cursor, right) = cursor.split();
-    format!(
-        "{left}{cursor}{right}",
-        left = new_style.apply_to(left),
-        cursor = style(cursor).reverse(),
-        right = new_style.apply_to(right)
-    )
-}
-
 pub trait Theme {
+    fn state_color(&self, state: &ThemeState) -> Style {
+        match state {
+            ThemeState::Active => Style::new().cyan(),
+            ThemeState::Cancel => Style::new().red(),
+            ThemeState::Submit => Style::new().bright().black(),
+            ThemeState::Error(_) => Style::new().yellow(),
+        }
+    }
+
+    fn state_symbol(&self, state: &ThemeState) -> String {
+        let color = self.state_color(state);
+        let green = Style::new().green();
+
+        match state {
+            ThemeState::Active => color.apply_to(S_STEP_ACTIVE),
+            ThemeState::Cancel => color.apply_to(S_STEP_CANCEL),
+            ThemeState::Submit => green.apply_to(S_STEP_SUBMIT),
+            ThemeState::Error(_) => color.apply_to(S_STEP_ERROR),
+        }
+        .to_string()
+    }
+
+    fn cursor_with_style(&self, cursor: &StringCursor, new_style: &Style) -> String {
+        let (left, cursor, right) = cursor.split();
+        format!(
+            "{left}{cursor}{right}",
+            left = new_style.apply_to(left),
+            cursor = style(cursor).reverse(),
+            right = new_style.apply_to(right)
+        )
+    }
+
     fn format_intro(&self, title: &str) -> String {
-        let color = state_color(&ThemeState::Submit);
+        let color = self.state_color(&ThemeState::Submit);
         format!(
             "{start_bar}  {title}\n{bar}",
             start_bar = color.apply_to(S_BAR_START),
@@ -89,12 +89,12 @@ pub trait Theme {
     }
 
     fn format_outro(&self, message: &str) -> String {
-        let color = state_color(&ThemeState::Submit);
+        let color = self.state_color(&ThemeState::Submit);
         format!("{bar}  {message}\n", bar = color.apply_to(S_BAR_END))
     }
 
     fn format_cancel(&self, message: &str) -> String {
-        let color = state_color(&ThemeState::Submit);
+        let color = self.state_color(&ThemeState::Submit);
         format!(
             "{bar}  {message}",
             bar = color.apply_to(S_BAR_END),
@@ -105,7 +105,7 @@ pub trait Theme {
     fn format_header(&self, state: &ThemeState, prompt: &str) -> String {
         format!(
             "{state_symbol}  {prompt}\n",
-            state_symbol = state_symbol(state)
+            state_symbol = self.state_symbol(state)
         )
     }
 
@@ -117,31 +117,34 @@ pub trait Theme {
         };
 
         let input = &match state {
-            ThemeState::Active | ThemeState::Error(_) => cursor_parts_with_style(cursor, new_style),
+            ThemeState::Active | ThemeState::Error(_) => self.cursor_with_style(cursor, new_style),
             _ => new_style.apply_to(cursor).to_string(),
         };
 
-        format!("{bar}  {input}\n", bar = state_color(state).apply_to(S_BAR))
+        format!(
+            "{bar}  {input}\n",
+            bar = self.state_color(state).apply_to(S_BAR)
+        )
     }
 
     fn format_placeholder(&self, state: &ThemeState, cursor: &StringCursor) -> String {
         let new_style = &Style::new().dim();
 
         let placeholder = &match state {
-            ThemeState::Active | ThemeState::Error(_) => cursor_parts_with_style(cursor, new_style),
+            ThemeState::Active | ThemeState::Error(_) => self.cursor_with_style(cursor, new_style),
             _ => new_style.apply_to(cursor).to_string(),
         };
 
         format!(
             "{bar}  {placeholder}\n",
-            bar = state_color(state).apply_to(S_BAR)
+            bar = self.state_color(state).apply_to(S_BAR)
         )
     }
 
     fn format_footer(&self, state: &ThemeState) -> String {
         format!(
             "{}\n", // '\n' vanishes by style applying, thus exclude it from styling
-            state_color(state).apply_to(match state {
+            self.state_color(state).apply_to(match state {
                 ThemeState::Active => format!("{S_BAR_END}"),
                 ThemeState::Cancel => format!("{S_BAR_END}  Operation cancelled."),
                 ThemeState::Submit => format!("{S_BAR}"),
