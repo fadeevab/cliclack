3b5104bc7e31d57a7c278d6e6857fbc53bcee185
Samuel Ainsworth <skainsworth@gmail.com>
2024-01-14 14:38:25 -0800

    Apply suggestions from code review
    Co-authored-by: Alexander Fadeev <fadeevab.com@gmail.com>

--- examples/basic.rs
+++ examples/basic.rs
@@ -3,13 +3,14 @@ use std::{thread, time::Duration};
 use console::style;
 
 fn main() -> std::io::Result<()> {
-    // Set a no-op Ctrl-C handler so that Ctrl-C results in a
-    // `term.read_key()` error instead of terminating the process. You can skip
+    // Set a no-op Ctrl-C handler so that Ctrl-C results in
+    // `Esc` behavior because of a `term.read_key()` error
+    // instead of terminating the process. You can skip
     // this step if you have your own Ctrl-C handler already set up.
     //
     // We cannot (easily) handle this at the library level due to
     // https://github.com/Detegr/rust-ctrlc/issues/106#issuecomment-1887793468.
-    ctrlc::set_handler(move || {}).expect("Error setting Ctrl-C handler");
+    ctrlc::set_handler(move || {}).expect("setting Ctrl-C handler");
 
     cliclack::clear_screen()?;
 
