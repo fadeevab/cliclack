9db8f20b76cab6b249f3016af25939c07434c0b2
Cyle Witruk <cyle.witruk@outlook.com>
2024-03-27 17:26:35 +0100

    docs: add comments

--- examples/downloadbar.rs
+++ examples/downloadbar.rs
@@ -4,32 +4,50 @@ use rand::{thread_rng, Rng};
 use std::{sync::mpsc::channel, time::Duration};
 
 fn main() -> std::io::Result<()> {
+    // Total number of bytes to simulate downloading.
     let total_bytes = 5_000_000;
+
+    // Create a MPSC channel which will be used by the Ctrl-C handler to
+    // asynchronously send a signal to the loop down below, allowing us to cancel
+    // the operation.
     let (tx, rx) = channel();
 
+    // Set the Ctrl-C handler to send a signal to the channel.
     ctrlc::set_handler(move || {
         let _ = tx.send(true);
     })
-        .expect("setting Ctrl-C handler");
+    .expect("setting Ctrl-C handler");
 
+    // Clear the screen and print the header + a remark.
     clear_screen()?;
     intro(style(" progressbar ").on_cyan().black())?;
     log::remark("Press Esc, Enter, or Ctrl-C")?;
 
+    // Create a new download progress bar
     let mut progressbar = downloadbar();
-    progressbar.start(total_bytes, "Installation");
+    // Start the progress bar with the total number of bytes and a label
+    progressbar.start(total_bytes, "Downloading, please wait...");
 
-    let timeout = Duration::from_millis(100);
+    // Loop until the progress bar reaches the total number of bytes
     while progressbar.get_progress() < total_bytes {
-        if rx.recv_timeout(timeout).is_ok() { 
+        // Use a random timeout to simulate some work.
+        let timeout = Duration::from_millis(thread_rng().gen_range(10..150));
+
+        // Check if we received a signal from the channel (the Ctrl-C handler
+        // registered above). We use this as our "thread::sleep()" operation
+        // as well, to simulate work.
+        if rx.recv_timeout(timeout).is_ok() {
+            // If a message is received, cancel the progress bar and display an outro message
             progressbar.cancel("Installation")?;
             outro("Cancelled")?;
             return Ok(());
         }
-        
+
+        // Increment the progress bar with a random number of bytes
         progressbar.increment(thread_rng().gen_range(1_000..200_000));
     }
 
+    // Stop the progress bar and display a completion message
     progressbar.stop("Installation");
     outro("Done!")?;
 
--- examples/progressbar.rs
+++ examples/progressbar.rs
@@ -5,30 +5,49 @@ use console::style;
 use rand::{thread_rng, Rng};
 
 fn main() -> std::io::Result<()> {
+    // Create a MPSC channel which will be used by the Ctrl-C handler to
+    // asynchronously send a signal to the loop down below, allowing us to cancel
+    // the operation.
     let (tx, rx) = channel();
 
+    // Set the Ctrl-C handler to send a signal to the channel.
     ctrlc::set_handler(move || {
         let _ = tx.send(true);
     })
         .expect("setting Ctrl-C handler");
 
+    // Clear the screen and print the header.
     clear_screen()?;
     intro(style(" progressbar ").on_cyan().black())?;
     log::remark("Press Esc, Enter, or Ctrl-C")?;
 
+    // Create a new progressbar and set the text to "Installation".
     let mut progressbar = progressbar();
     progressbar.start(100, "Installation");
 
+    // Simulate doing some stuff....
     for _ in 0..100 {
+        // Use a random timeout to simulate some work.
         let timeout = Duration::from_millis(thread_rng().gen_range(10..75));
+
+        // Check if we received a signal from the channel (the Ctrl-C handler
+        // registered above). We use this as our "thread::sleep()" operation
+        // as well, to simulate work.
         if rx.recv_timeout(timeout).is_ok() { 
+            // If a message is received, cancel the progress bar and display an outro message
             progressbar.cancel("Installation")?;
             outro("Cancelled")?;
             return Ok(());
         }
+
+        // Otherwise, we increase the progressbar by the delta. In this case we're
+        // using a fixed delta of 1, but otherwise this would be the _change in
+        // progress_ from the last iteration to this one.
         progressbar.increment(1);
     }
 
+    // Once we're done, we stop the progressbar and print the outro message.
+    // This removes the progressbar and prints the message to the terminal.
     progressbar.stop("Installation");
     outro("Done!")?;
 
