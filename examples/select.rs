e703b59ff5d8337e3f3cdc72eaac1ba12cb7de3d
antoinePutmans <113629846+antoinePutmans@users.noreply.github.com>
2024-04-22 16:47:13 +0200

    style: running rustfmt on changed files

--- examples/select.rs
+++ examples/select.rs
@@ -2,17 +2,16 @@ use cliclack::select;
 
 fn main() {
     let selected = select("Select a word")
-        .item("hello","hello","hi")
-        .item("world","world","world")
-        .item("how","how","how")
-        .item("are","are","are")
-        .item("you","you","you")
-        .item("hello YOU","hello YOU","hello YOU")
+        .item("hello", "hello", "hi")
+        .item("world", "world", "world")
+        .item("how", "how", "how")
+        .item("are", "are", "are")
+        .item("you", "you", "you")
+        .item("hello YOU", "hello YOU", "hello YOU")
         .enable_filter()
         .interact();
 
     if let Ok(val) = selected {
         println!("you chose: {}", val);
     }
-
 }
