b4a13f024b2171967c8f4ae08ad7a5bbc1b744d5
Louis <836250617@qq.com>
2024-05-31 22:32:27 +0800

    revert: cancel doc_auto_cfg in this PR

--- build.rs
+++
@@ -1,9 +0,0 @@
-use rustc_version::{version_meta, Channel};
-
-fn main() {
-    // Set cfg flags depending on release channel
-    if let Channel::Nightly = version_meta().unwrap().channel {
-        println!("cargo::rustc-check-cfg=cfg(CHANNEL_NIGHTLY)");
-        println!("cargo:rustc-cfg=CHANNEL_NIGHTLY");
-    }
-}
